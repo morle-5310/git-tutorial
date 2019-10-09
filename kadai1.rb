268036923d00e54ba23f7007b2e5f16839c6e2ed
morle-5310 <j1602@matsue-ct.jp>
2019-10-09 10:39:00 +0900

    second commit

--- kadai1.rb
+++ kadai1.rb
@@ -1,16 +1,13 @@
-#include<stdio.h>
-int main() {
-  int i;
-  
+#! /usr/bin/env ruby 
   for (i = 1;i <= 100;i++) {
     if (i % 3 == && i % 5 == 0){
-      printf("FizzBuzz\n");
+      print("FizzBuzz\n");
     } else if (i % 3 == 0){
-      printf("Fizz\n");
+      print("Fizz\n");
     } else if (i % 5 == 0){
-      printf("Buzz\n");
+      print("Buzz\n");
     } else {
-      printf("%d\n",i);
+      print("%d\n",i);
     }
   }
   return 0;
