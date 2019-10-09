351c52225696e7ec0fee7ad0bcf9bbdd2dd6d137
morle-5310 <j1602@matsue-ct.jp>
2019-10-09 10:54:03 +0900

    fifth commit

--- kadai1.rb
+++ kadai1.rb
@@ -1,6 +1,6 @@
 #! /usr/bin/env ruby 
   for i in 1..100 
-    if ( i % 3 == && i % 5 == 0) then 
+    if ( i % 3 == and i % 5 == 0) then 
       print("FizzBuzz\n")
     else if (i % 3 == 0) then 
       print("Fizz\n")
