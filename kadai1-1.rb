855107f93a885d2261a8ad2978e9fbe3d396209a
morle-5310 <j1602@matsue-ct.jp>
2019-10-09 11:20:49 +0900

    third commit for kadai1-1.rb

--- kadai1-1.rb
+++ kadai1-1.rb
@@ -1,12 +1,12 @@
 #! /usr/bin/env ruby
-  for (i = 1; i <= 100; i++) {
-    if (i % 3 == 0 && i % 5 == 0){
+  for i in 1..100 
+    if (i % 3 == 0 && i % 5 == 0) then 
       print("FizzBuzz\n")
-    } else if (i % 3 == 0) {
+    elsif (i % 3 == 0) then 
       print("Fizz\n")
-    } else if (i % 5 == 0) {
+    elsif (i % 5 == 0) then 
       print("Buzz\n")
-    } else {
+    else 
       print(i,"\n")
-    }
-  }
+    end 
+  end 
