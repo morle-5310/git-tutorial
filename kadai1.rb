6de75c3c673569fa86a9e0aa19c0c3ff70782112
morle-5310 <j1602@matsue-ct.jp>
2019-10-09 11:01:12 +0900

    ninth commit

--- kadai1.rb
+++ kadai1.rb
@@ -2,9 +2,9 @@
   for i in 1..100 
     if ( i % 3 == 0 && i % 5 == 0) then 
       print("FizzBuzz\n")
-    else if (i % 3 == 0) then 
+    elsif (i % 3 == 0) then 
       print("Fizz\n")
-    else if (i % 5 == 0) then 
+    elsif (i % 5 == 0) then 
       print("Buzz\n")
     else 
       print("%d\n",i)
